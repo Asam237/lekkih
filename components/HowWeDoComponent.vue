a807dc68dc89cbc27383be559d998c906223cb07
Asam237 <abbasalimokolo@gmail.com>
2022-11-25 20:15:26 +0100

    feat: update howwedocomponent

--- components/HowWeDoComponent.vue
+++ components/HowWeDoComponent.vue
@@ -1,26 +1,22 @@
 <template>
-    <div class="md:h-[60vh] mt-12 md:mt-24">
+    <div class="md:h-[60vh] my-14 md:my-36">
         <div class="container">
             <div class="flex flex-col items-center justify-center md:mx-[5vw]">
-                <h1 class="text-[34px] md:text-[68px] text-center md:mx-[17vw]">Voici comment nous réparons les choses
+                <h1
+                    class="text-[34px] md:text-[68px] text-center md:mx-[17vw] font-normal leading-10 md:leading-[70px]">
+                    Voici comment nous réparons
+                    les choses
                 </h1>
-                <div class="mt-6 md:mt-12">
-                    <p class="md:mx-[15vw] text-center text-3xl leading-10" style="font-family: 'Plantin;">Nous
-                        perturbons l'industrie en
-                        stockant
-                        les
-                        étagères des
-                        pharmacies
-                        sans
-                        paiement préalable.
-                        Nous utilisons la puissance collective de notre réseau de pharmacies pour négocier des prix
-                        plus
-                        bas avec les meilleurs fabricants.
-                        Nous exploitons la technologie pour éliminer les inefficacités et les fluctuations de prix
-                        qui
-                        empêchent les médicaments sur ordonnance d'atteindre les personnes malades.
-                    </p>
-                </div>
+                <h4
+                    class="md:mx-[15vw] text-center text-[20px] md:text-[30px] md:leading-[3rem] font-normal pt-6 md:pt-12">
+                    Nous
+                    perturbons
+                    l'industrie en
+                    stockant les étagères des pharmacies sans paiement préalable. Nous utilisons la puissance
+                    collective de notre réseau de pharmacies pour négocier des prix plus bas avec les meilleurs
+                    fabricants. Nous exploitons la technologie pour éliminer les inefficacités et les fluctuations
+                    de prix qui empêchent les médicaments sur ordonnance d'atteindre les personnes malades.
+                </h4>
             </div>
         </div>
     </div>
