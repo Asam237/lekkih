25755b788e8f8752c376d649aceaa96718ec4394
Asam237 <abbasalimokolo@gmail.com>
2022-11-25 20:29:49 +0100

    feat: update socials components

--- components/SocialsComponent.vue
+++ components/SocialsComponent.vue
@@ -2,19 +2,22 @@
     <div class="py-24 bg-[#fdc300]">
         <div class="container">
             <div>
-                <h1 class="font-[400] text-[34px] md:text-[68px] text-[#1f1f1f] text-center"
+                <h1 class="font-[400] text-[34px] md:text-[68px] text-[#1f1f1f] text-center leading-10 md:leading-[70px]"
                     style="font-family: 'Plantin Bold';">mPharma dans les médias</h1>
-                <div class="grid gap-12 grid-cols-1 md:grid-cols-2 mt-14 md:mt-28">
-                    <div v-for="(item, index) in cards" :key="index"
-                        class="bg-white shadow-sm p-10 md:p-20 flex flex-col items-center rounded-md">
-                        <h4 class="font-[400] text-[20px] leading-[24px] text-[#1f1f1f] md:text-[24px] text-center">{{
-                                item.title
-                        }}</h4>
-                        <p class="text-center text-[17px] leading-[26px] md:text-[21px] text-[#1f1f1f] my-6 md:my-12"
-                            style="font-family: 'Plantin Light';">{{
-                                    item.content
-                            }}</p>
-                        <p class="font-bold text-lg text-center">{{ item.footer }}</p>
+                <div class="md:w-3/4 container mx-auto">
+                    <div class="grid gap-12 grid-cols-1 md:grid-cols-2 mt-14 md:mt-28 content-center">
+                        <div v-for="(item, index) in cards" :key="index"
+                            class="bg-white shadow-sm p-10 md:p-20 flex flex-col items-center rounded-md">
+                            <h4 class="font-[400] text-[26px] leading-[24px] text-[#1f1f1f] md:text-[34px] text-center">
+                                {{
+                                        item.title
+                                }}</h4>
+                            <p class="text-center text-[17px] leading-[26px] md:text-[21px] text-[#1f1f1f] my-6 md:my-12"
+                                style="font-family: 'Plantin Light';">{{
+                                        item.content
+                                }}</p>
+                            <p class="font-bold text-lg text-center">{{ item.footer }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
