a9ed87cc7006c4910110519874d169ea6191e159
Asam237 <abbasalimokolo@gmail.com>
2022-11-25 20:00:29 +0100

    feat: update banner

--- components/BannerComponent.vue
+++ components/BannerComponent.vue
@@ -3,17 +3,14 @@
         class=" bg-no-repeat object-cover h-[70vh] md:h-[90vh] w-full bg-center bg-cover bg-[url('/assets/imgs/doctor.jpg')]">
         <div
             class="fade flex md:justify-end  justify-center text-center md:text-start h-[80vh] flex-col pt-[4rem] container">
-            <h1 class="text-[34px] md:text-[68px] font-[400] text-white leading-[35px] md:leading-[72px] md:w-3/4">Lorem
-                ipsum dolor sit,
-                amet consectetur
-                adipisicing elit. Laborum, ipsam.</h1>
-            <p class="fade text-[18px] md:text-[22px] leading-[30px] md:leading-[40px] md:w-1/2 mt-8 text-white">Lorem
-                ipsum
-                dolor sit amet
-                consectetur
-                adipisicing elit. Possimus
-                exercitationem officiis eum aliquid
-                officia similique.</p>
+            <h1 class="text-[34px] md:text-[68px] font-[400] text-white leading-[35px] md:leading-[72px] md:w-1/2">
+                De nombreux pays ne sont pas préparés.
+            </h1>
+            <h4 class="fade text-[18px] md:text-[22px] leading-[30px] md:leading-[40px] md:w-3/5 mt-8 text-white font-normal">
+                Après des décennies de priorité accordée au paludisme et au VIH, ce sont les nouveaux tueurs - comme le
+                cancer, le diabète et l'hypertension - qui font peser le plus lourd fardeau sur les systèmes de santé
+                africains.
+            </h4>
         </div>
     </div>
 </template>
